54f778fe256beba2cf4eb7e96ae9038d419040ea
Erik Young <erik_young@mac.com>
2022-07-22 21:31:46 -0400

    Merged styles dir fom use_inter... branch.

---
+++ styles/globals.js
@@ -0,0 +1,12 @@
+
+
+export const bodyStyled = `
+  body {
+    background: rgb(5,3,72);
+    background: linear-gradient(90deg, rgba(5,3,72,1) 0%, rgba(112,9,121,1) 64%, rgba(152,244,246,1) 100%);
+  };
+
+  a {
+    text-decoration: none;
+  }
+`;
